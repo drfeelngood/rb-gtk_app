38cd964ce60cc0edb36cc672a0c1d0b3805e9b81
drfeelngood <dan@dj-agiledev.com>
2011-12-29 19:51:03 -0600

    Added YARD doc and fixed the build_listview method.

--- lib/gtk_app/view_helpers.rb
+++ lib/gtk_app/view_helpers.rb
@@ -1,22 +1,32 @@
 module GtkApp
 module ViewHelpers
-  
-  def build_listview(widget_name, columns, options={}, &block) # :yields: index, header, column, renderer
+
+  # @param [Symbol] Widget name
+  # @param [Hash]   
+  # @param [Hash] options
+  # @option options [Proc] :formatter
+  # @option options [Array]
+  # @yield [iter, header, column, renderer]  Core listview elements are yielded for each column.
+  # @yieldparam [Integer] iter Column index
+  # @yieldparam [String] header Column header text
+  # @yieldparam [Gtk::TreeViewColumn] column The current column
+  # @yieldparam [Gtk::CellRenderer]  renderer Current column cell renderer
+  def build_listview(widget_name, columns, options={}, &block)
     list = self.send(:"#{widget_name}")
     list.model = Gtk::ListStore.new(*columns.values)
     columns.each_with_index do |keyval, index|
       header, data_type = keyval
-      renderer, attrs = case data_type
-      when String, Integer
+      renderer, attrs = case "#{data_type}".to_sym
+      when :String, :Integer
         [Gtk::CellRendererText.new, :text => index]
-      when TrueClass
+      when :TrueClass
         toggle = Gtk::CellRendererToggle.new
         toggle.signal_connect('toggled') do |widget, path|
           iter = list.model.get_iter(path)
           iter[index] = !iter[index]
         end
         [toggle, :active => index]
-      when Gtk::ListStore
+      when :'Gtk::ListStore'
         _renderer = Gtk::CellRendererCombo.new
         model = Gtk::ListStore.new(String)
         _renderer.signal_connect("edited") do |cell, path, text|
@@ -40,17 +50,17 @@ module ViewHelpers
       list.append_column(column)
     end
   end
-  
-  # Set *widgets sensitive property to true
-  #== Example
-  # @view.sensitize(:txtFoo, :cmbBar)
+
+  # Set widget(s) sensitivity property to true
+  # @overload sensitize(:lblFoo, :txtFoo)
+  # @overload sensitize(:cmbBar)
   def sensitize(*widgets)
     widgets.each { |w| self["#{w}"].sensitive = true }
   end
-  
-  # Set *widgets sensitive property to false
-  #== Example
-  # @view.desensitize(:txtFoo, :cmbBar)
+
+  # Set widget(s) sensitivity property to false
+  # @overload desensitize(:lblFoo, :lblBar)
+  # @overload desensitize(:cmbBar)
   def desensitize(*widgets)
     widgets.each { |w| self["#{w}"].sensitive = false }
   end
