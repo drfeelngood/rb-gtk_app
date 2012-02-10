4c5c06a8a333c386ca41f5c47bb848894a9d20fc
Daniel <dan@dj-agiledev.com>
2012-02-09 19:58:26 -0600

    Updated to yard doc.

--- lib/gtk_app/controller.rb
+++ lib/gtk_app/controller.rb
@@ -3,14 +3,18 @@ class Controller
   include GtkApp::Helpers
   include GtkApp::SignalSupport
 
-  attr_accessor :model, :view
+  # @attribute [rw] model
+  attr_accessor :model
+  # @attr_accessor [rw] view
+  attr_accessor :view
 
   def initialize(&block)
     instance_eval(&block) if block_given?
-    
+
     establish_signal_connections
   end
 
+  # @param [Boolean] with_validations
   def quit(with_validations=true)
     # TODO: if with_validations
     # end
