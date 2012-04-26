29620d97e38966d6d1ce1e53e3a441925fcf86d8
Aaron <alkrauss48@gmail.com>
2012-04-26 14:46:23 -0500

    full functioning spell checker sans seg fault

--- lib/gtk_app/text_edit_support.rb
+++ lib/gtk_app/text_edit_support.rb
@@ -17,49 +17,44 @@ module TextEditSupport
       setup_signals
     end
 
+    # sets up the signals like a boss
     def setup_signals
-      menu = Gtk::Menu.new
       signal_connect('button-press-event') do |me, event|
         if event.button == 3
-          menu = Gtk::Menu.new
-          puts "I have been right clicked!"
           coords = window_to_buffer_coords(get_window_type(event.window), event.x, event.y)
-          puts "#{coords[0]} #{coords[1]}"
           click_iter = get_iter_at_location(coords[0], coords[1])
           mark_click = buffer.create_mark('mark_click', click_iter, false)
         end
-         false #let gtk process this event, too. we don't want to eat any events
+         false #let gtk process this event too. we don't want to eat any events
       end
 
       signal_connect('populate_popup') do |me, p_menu|
-        puts "called the populate popup signal"
-        spell_menu_item = Gtk::MenuItem.new("Spelling Suggestions")
         menu = Gtk::Menu.new
+        spell_menu_item = Gtk::MenuItem.new("Spelling Suggestions")
         mark_click = me.buffer.get_mark('mark_click')
         iters = get_word_iters(mark_click)
-        get_suggestions_menu(menu, iters)
+        add_suggestions_to_menu(menu, iters)
         spell_menu_item.set_submenu(menu)
         p_menu.insert(spell_menu_item, 0)
         p_menu.show_all
-        false
-      end
-
-      #app seg faults without the menu destroying properly. This still doesn't work.
-      signal_connect('destroy') do |me|
-        puts "hit the destroy signal"
-        menu.destroy
+        false #let gtk process this event too
       end
     end
 
+    # @param mark_click [Gtk::TextMark] mark at where the right-click occurs
+    # @return [Array] start and end iter of the word of the passed mark
     def get_word_iters(mark_click)
-      s_iter = buffer.get_iter_at_mark(mark_click)
-      s_iter.forward_word_end
-      e_iter = s_iter.clone
-      s_iter.backward_word_start
+      iter = buffer.get_iter_at_mark(mark_click)
+      s_iter, e_iter = iter.clone, iter.clone
+      s_iter.backward_word_start unless s_iter.starts_word?
+      e_iter.forward_word_end unless e_iter.ends_word?
       [s_iter, e_iter]
     end
 
-    def get_suggestions_menu(menu, iters)
+    # @param menu [Gtk::Menu] menu that is to be added upon
+    # @param iters [Array of Gtk::TextIters] contains start and end iters of word
+    # @return void - just adds the suggestions to the menu
+    def add_suggestions_to_menu(menu, iters)
       @spell_check = Aspell.new(DEFAULT_LANG)
       word = buffer.get_text(iters[0], iters[1])
       suggestions = @spell_check.suggest(word)[0...5]
@@ -76,9 +71,7 @@ module TextEditSupport
           end
         end
       end
-      menu.show_all
     end
-
   end
 end
 end
